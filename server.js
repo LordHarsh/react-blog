c6c52e7c0a55cf939d23c2bf6f819b94794a4033
Harsh Banka <harshbanka321@gmail.com>
2023-06-28 02:28:30 +0530

    updated

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGO_URI);
+mongoose.Promise = global.Promise;
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+app.get('/blogs', async (req, res) => {
+  const blogs = await Blog.find({});
+  res.send(blogs);
+});
+
+app.get('/blogs/:id', async (req, res) => {
+  const blog = await Blog.findById(req.params.id);
+  res.send(blog);
+});
+
+app.post('/blogs', async (req, res) => {
+  const blog = new Blog(req.body);
+  await blog.save();
+  res.send(blog);
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
